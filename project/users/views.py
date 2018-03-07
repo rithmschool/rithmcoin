34ae42b1cd3f03ee451a4ce44f79668e84e33e17
Matt Lane <lane.matthew@gmail.com>
2018-03-07 10:12:37 -0800

    login/signup working, basic account page creation

--- project/users/views.py
+++ project/users/views.py
@@ -1,25 +1,44 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from project.models import User
 from project import db
+from flask_login import login_user, logout_user, current_user, login_required
+from sqlalchemy.exc import IntegrityError
 
 users_blueprint = Blueprint('users', __name__, template_folder = 'templates')
 
 @users_blueprint.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        pass
+        found_user = User.authenticate(request.form['email'],
+                                       request.form['password'])
+        if found_user:
+            login_user(found_user)
+            return redirect(url_for('users.account'))
+        flash("Invalid credentials")
     return render_template("login.html")
 
 @users_blueprint.route('/signup', methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
-        pass
+        try:
+            new_user = User(request.form['name'],
+                            request.form['email'],
+                            request.form['password'])
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user) 
+        except IntegrityError as e:
+            flash("Email already taken")
+            return render_template('signup.html')
+        return redirect(url_for('users.account'))
     return render_template("signup.html")
 
 @users_blueprint.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('root'))
 
 @users_blueprint.route('/account')
+@login_required
 def account():
-    pass
+    return render_template("account.html")
