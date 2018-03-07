c3b3de4f024498fcab9f91e57fd64338ab89bd8b
Matt Lane <lane.matthew@gmail.com>
2018-03-07 13:10:54 -0800

    adding cors

--- project/__init__.py
+++ project/__init__.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_cors import CORS
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+CORS(app)
 
 import os
 
