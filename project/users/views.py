bda01c2649d4fcb4324fc5ecb2f4eb8f6d6c8130
Matt Lane <lane.matthew@gmail.com>
2018-03-12 16:20:38 -0700

    adding support for form hijacking

--- project/users/views.py
+++ project/users/views.py
@@ -4,7 +4,8 @@ from project import db
 from flask_login import login_user, logout_user, current_user, login_required
 from sqlalchemy.exc import IntegrityError
 
-users_blueprint = Blueprint('users', __name__, template_folder = 'templates')
+users_blueprint = Blueprint('users', __name__, template_folder='templates')
+
 
 @users_blueprint.route('/login', methods=["GET", "POST"])
 def login():
@@ -17,26 +18,27 @@ def login():
         flash("Invalid credentials")
     return render_template("login.html")
 
+
 @users_blueprint.route('/signup', methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
         try:
-            new_user = User(request.form['name'],
-                            request.form['email'],
-                            request.form['password'])
+            new_user = User(**dict(request.form.items()))
             db.session.add(new_user)
             db.session.commit()
-            login_user(new_user) 
+            login_user(new_user)
         except IntegrityError as e:
             flash("Email already taken")
             return render_template('signup.html')
         return redirect(url_for('users.account'))
     return render_template("signup.html")
 
+
 @users_blueprint.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('root'))
+
 
 @users_blueprint.route('/account')
 @login_required
